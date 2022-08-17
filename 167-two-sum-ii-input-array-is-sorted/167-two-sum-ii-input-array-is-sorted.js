cca6184136b727963ed8de870d3990780ef1b953
Rajat Prasad <60283721+rajatprasad@users.noreply.github.com>
2022-08-17 21:23:10 +0530

    Time: 102 ms (38.67%), Space: 42.5 MB (96.17%) - LeetHub

---
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    let left = 0, right = numbers.length - 1;
+    
+    while(left < right) {
+        let current = numbers[left] + numbers[right];
+        
+        if(current > target)
+            right = right - 1;
+        else if(current < target)
+            left = left + 1;
+        else
+            return [left + 1, right + 1];
+    }
+    return [];
+};
