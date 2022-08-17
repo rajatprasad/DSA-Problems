0ee4293b02f78fe51fcb0026ceeaff06852e2652
Rajat Prasad <60283721+rajatprasad@users.noreply.github.com>
2022-08-17 20:19:52 +0530

    Time: 103 ms (28.92%), Space: 41.7 MB (93.95%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function(m, n) {
+    if(m === 0 || n === 0) return 0;
+    if(m === 1 || n === 1) return 1;
+    
+    // initialise DP with base cases
+   let dp = Array(m).fill(0).map(() => Array(n).fill(1));
+    
+    for(let i = 1; i < m; i++) {
+        for(let j = 1; j < n; j++) {
+            dp[i][j] = dp[i-1][j] + dp[i][j-1];
+        }
+    }
+    
+    // return value for bottom right
+    return dp[m-1][n-1];
+};
