37cedee289bdd162650061e2e639b280ae304cc8
Rajat Prasad <60283721+rajatprasad@users.noreply.github.com>
2022-08-18 15:13:28 +0530

    Time: 212 ms (62.05%), Space: 52 MB (91.06%) - LeetHub

--- 15-3sum/15-3sum.js
+++ 15-3sum/15-3sum.js
@@ -5,9 +5,20 @@
 var threeSum = function(nums) {
     let res = [];
     
+// having the numbers in ascending order will make this problem much easier.
+// also, knowing the overall problem  will take at least O(N^2) time, we can
+// afford the O(NlogN) sort operation
+    
     nums.sort((a,b) => a - b);
     
+
     for(let i = 0; i < nums.length; i++) {
+        
+        // once this number hits 0, there's no need to go further since
+		// positive numbers cannot sum to a negative number (optional)
+		if (nums[i] > 0) break
+        
+        // we don't want repeats, so skip numbers we've already seen
         if(nums[i] === nums[i - 1])
             continue
         
