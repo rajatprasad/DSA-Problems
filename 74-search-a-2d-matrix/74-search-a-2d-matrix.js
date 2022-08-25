d041ab46ee7bcd18f08f3eb8778d8958598211bd
Rajat Prasad <60283721+rajatprasad@users.noreply.github.com>
2022-08-25 17:37:58 +0530

    Time: 93 ms (44.66%), Space: 41.9 MB (88.49%) - LeetHub

---
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.js
@@ -0,0 +1,48 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+
+// var searchMatrix = function(matrix, target) {
+//     let j = matrix[0].length - 1, i = 0;
+
+//   while(j >= 0 && i < matrix.length) {
+//     if(matrix[i][j] === target) return true
+
+//     if(matrix[i][j] > target)
+//       j--
+//     else
+//       i++;
+//   }
+//   return false;
+// };
+
+var searchMatrix = function(matrix, target) {
+  let start = 0, end = (matrix.length * matrix[0].length) - 1
+    
+    while(start <= end) {
+        let mid = Math.floor((start + end) / 2)
+        let midNum = 
+            matrix[Math.floor(mid / matrix[0].length)][mid % matrix[0].length]
+        
+        if(midNum === target) return true    
+        else if(midNum < target) start = mid + 1
+        else end = mid - 1
+    }
+    return false
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
