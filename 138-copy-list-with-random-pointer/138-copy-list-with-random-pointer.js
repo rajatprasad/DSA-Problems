862f9f045ee28791e95a7200d4ad7a70d3c8fd95
Rajat Prasad <60283721+rajatprasad@users.noreply.github.com>
2022-09-06 14:06:50 +0530

    Time: 92 ms (51.09%), Space: 43.9 MB (78.96%) - LeetHub

---
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.js
@@ -0,0 +1,32 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, next, random) {
+ *    this.val = val;
+ *    this.next = next;
+ *    this.random = random;
+ * };
+ */
+
+/**
+ * @param {Node} head
+ * @return {Node}
+ */
+var copyRandomList = function(head) {
+    let map = new Map();
+    
+    let curr = head;
+    
+    while(curr) {
+        map.set(curr, new Node(curr.val,null,null));
+        curr = curr.next;
+    }
+    
+    curr = head;
+    
+    while(curr) {
+        map.get(curr).next = map.get(curr.next) || null;
+        map.get(curr).random = map.get(curr.random) || null;
+        curr = curr.next;
+    }
+    return map.get(head);
+};
