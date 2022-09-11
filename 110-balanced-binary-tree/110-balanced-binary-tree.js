c8deb1110e1ea748b66a03d6b563b107b4a91d9a
Rajat Prasad <60283721+rajatprasad@users.noreply.github.com>
2022-09-11 14:28:18 +0530

    Time: 114 ms (51.16%), Space: 47 MB (75.61%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.js
+++ 110-balanced-binary-tree/110-balanced-binary-tree.js
@@ -11,14 +11,22 @@
  * @return {boolean}
  */
 var isBalanced = function(root) {
-    
-    let dfs = function(node) {
-        if (!node) return 0;
-        let left = 1 + dfs(node.left);
-        let right = 1 + dfs(node.right);
-        if (Math.abs(left - right) > 1) return Infinity;
-        return Math.max(left, right);
+    let result = true;
+    function dfs(node) {
+        if (!node) {
+            return 0;
+        }
+        
+        const left = dfs(node.left);
+        const right = dfs(node.right);
+        if (Math.abs(left - right) > 1) {
+            result = false;
+        }
+        
+        return 1 + Math.max(left, right);
     }
     
-    return dfs(root)==Infinity?false:true;
+    dfs(root);
+    
+    return result;
 };
