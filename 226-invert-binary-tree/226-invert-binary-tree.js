48ec2c24e702bc122eb44e1ad85040cab508af04
Rajat Prasad <60283721+rajatprasad@users.noreply.github.com>
2022-09-11 12:25:05 +0530

    Time: 102 ms (26.23%), Space: 42.4 MB (56.74%) - LeetHub

--- 226-invert-binary-tree/226-invert-binary-tree.js
+++ 226-invert-binary-tree/226-invert-binary-tree.js
@@ -10,9 +10,11 @@
  * @param {TreeNode} root
  * @return {TreeNode}
  */
+
+// Recursion
 var invertTree = function(root) {
     if(!root) 
-        return root;
+        return null;
     
     let left = root.left;
     root.left = invertTree(root.right);
@@ -20,3 +22,34 @@ var invertTree = function(root) {
     
     return root;
 };
+
+
+// // DFS
+// function invertTree(root) {
+//   const stack = [root];
+
+//   while (stack.length) {
+//     const n = stack.pop();
+//     if (n != null) {
+//       [n.left, n.right] = [n.right, n.left];
+//       stack.push(n.left, n.right);
+//     }
+//   }
+
+//   return root;
+// }
+
+// // BFS
+// function invertTree(root) {
+//   const queue = [root];
+
+//   while (queue.length) {
+//     const n = queue.shift();
+//     if (n != null) {
+//       [n.left, n.right] = [n.right, n.left];
+//       queue.push(n.left, n.right);
+//     }
+//   }
+
+//   return root;
+// }
