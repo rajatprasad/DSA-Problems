5da55326b7cdf16463a5f44da59dcb420efcc3fa
Rajat Prasad <60283721+rajatprasad@users.noreply.github.com>
2022-09-14 12:46:02 +0530

    Time: 161 ms (35.84%), Space: 51.1 MB (42.84%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -1,19 +1,19 @@
 
-// var containsDuplicate = function(nums) {
-//     const map = {};
-//     for(let i=0; i < nums.length; i++ ) {
-//         if(map[nums[i]] !== undefined) {
-//             return true
-//         } else {
-//             map[nums[i]] = i
-//         }
-//     }           
-//         return false
-// };
+var containsDuplicate = function(nums) {
+    const map = {};
+    for(let i=0; i < nums.length; i++ ) {
+        if(map[nums[i]] !== undefined) {
+            return true
+        } else {
+            map[nums[i]] = i
+        }
+    }           
+        return false
+};
 
 
 //Pass the array into a Set() (which removes duplicates) and then compare its size to the original array.
 
-var containsDuplicate = function(nums) {
-    return new Set(nums).size !== nums.length;
-}
+// var containsDuplicate = function(nums) {
+//     return new Set(nums).size !== nums.length;
+// }
