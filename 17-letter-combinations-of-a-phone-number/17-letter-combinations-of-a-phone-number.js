15a57edcc05030bc015f2faca86fdd8f1927bf05
Rajat Prasad <60283721+rajatprasad@users.noreply.github.com>
2022-09-15 20:41:20 +0530

    Time: 100 ms (27.51%), Space: 42 MB (55.25%) - LeetHub

---
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+var letterCombinations = function(digits) {
+    
+    let res = [];
+    let combination = "";
+    let index = 0;
+    let mapping = ["", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"];
+    
+    if(digits.length == 0) return res;
+    
+    helper(digits, index, combination, mapping, res);
+    return res;
+};
+
+ function helper(digits, index, combination, mapping, res) {
+        if(index >= digits.length) {
+            res.push(combination);
+            return;
+        }
+        
+        let num = Number(digits[index]);
+        let value = mapping[num];
+        
+        for(let i = 0; i < value.length; i++) {
+            let temp =  combination + value[i]
+            helper(digits, index + 1, temp, mapping, res);
+            // temp.splice(-1, 1);
+        }
+    }
