2614013a087659ab5f575c45d8a7a6c2c6605274
Rajat Prasad <60283721+rajatprasad@users.noreply.github.com>
2022-09-20 00:35:05 +0530

    Time: 112 ms (25.02%), Space: 42.9 MB (86.72%) - LeetHub

--- 78-subsets/78-subsets.js
+++ 78-subsets/78-subsets.js
@@ -2,28 +2,73 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-var subsets = function(nums) {
+// var subsets = function(nums) {
     
-    let subset = [];
-    let res = [];
-    let index = 0;
+//     let subset = [];
+//     let res = [];
+//     let index = 0;
     
-    helper(nums, index, subset);
-    return res;
+//     helper(nums, index, subset);
+//     return res;
  
     
-    function helper(nums, index, subset) {
-        // base case
-        if(index == nums.length) {
-            res.push(subset);
-            return;
+//     function helper(nums, index, subset) {
+//         // base case
+//         if(index == nums.length) {
+//             res.push(subset);
+//             return;
+//         }
+        
+//         // exclude
+//         helper(nums, index+1, subset);
+        
+//         // include
+//         helper(nums, index+1, [...subset, nums[index]]);
+//     }
+    
+// };
+
+
+
+var subsets = function(nums) {
+    let res = [];
+    let subset = [];
+    
+    function solve(i) {
+        if(i >= nums.length) {
+            res.push(subset.slice());
+            return;            
         }
         
-        // exclude
-        helper(nums, index+1, subset);
-        
         // include
-        helper(nums, index+1, [...subset, nums[index]]);
+        subset.push(nums[i]);
+        solve(i+1);
+        
+        //exclude
+        subset.pop();
+        solve(i+1);
+        
     }
     
-};
+    solve(0);
+    return res;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
