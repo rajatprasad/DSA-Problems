5227a3b66909a57c899ccfe1cecf10c355e4cc11
Rajat Prasad <60283721+rajatprasad@users.noreply.github.com>
2022-09-28 23:15:05 +0530

    Time: 127 ms (14.59%), Space: 43.9 MB (26.62%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} stones
+ * @return {number}
+ */
+var lastStoneWeight = function(stones) {
+    if(stones.length < 2) return stones;
+    stones.sort((a,b) => a-b);
+    let a = stones.pop();
+    let b = stones.pop();
+    stones.push(Math.abs(a-b));
+    return lastStoneWeight(stones);
+};
