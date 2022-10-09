5677bddc647f873de44b4115264365331bf544b7
Rajat Prasad <60283721+rajatprasad@users.noreply.github.com>
2022-10-09 16:34:54 +0530

    Time: 120 ms (67.10%), Space: 43.1 MB (79.42%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.js
+++ 242-valid-anagram/242-valid-anagram.js
@@ -3,32 +3,76 @@
  * @param {string} t
  * @return {boolean}
  */
-var isAnagram = function(s, t) {
-    if (s.length !== t.length) {
-        return false;
-    }
+// var isAnagram = function(s, t) {
+//     if (s.length !== t.length) {
+//         return false;
+//     }
     
-    let sMap = {};
-    let tMap = {};
+//     let sMap = {};
+//     let tMap = {};
     
-    for (let i = 0; i < s.length; i++) {
-        if (sMap[s[i]] !== undefined) {
-            sMap[s[i]]++;
-        } else {
-            sMap[s[i]] = 1;
-        }
+//     for (let i = 0; i < s.length; i++) {
+//         if (sMap[s[i]] !== undefined) {
+//             sMap[s[i]]++;
+//         } else {
+//             sMap[s[i]] = 1;
+//         }
         
-        if (tMap[t[i]] !== undefined) {
-            tMap[t[i]]++;
-        } else {
-            tMap[t[i]] = 1;
-        }
+//         if (tMap[t[i]] !== undefined) {
+//             tMap[t[i]]++;
+//         } else {
+//             tMap[t[i]] = 1;
+//         }
+//     }
+    
+//     for (let k in sMap) {
+//         if (sMap[k] !== tMap[k])
+//             return false;
+//         }
+        
+//     return true;
+// };
+
+var isAnagram = function(s, t) {
+    if(s.length !== t.length)
+        return false;
+    
+    let map = {};
+    
+    for(let i = 0; i < s.length; i++) {
+        if(map[s[i]])
+            map[s[i]]++;
+        else
+            map[s[i]] = 1;
     }
     
-    for (let k in sMap) {
-        if (sMap[k] !== tMap[k]) {
+    for(i = 0; i < t.length; i++) {
+        if(map[t[i]])
+           map[t[i]]--;
+        else
             return false;
-        }
     }
+    
     return true;
-};
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
